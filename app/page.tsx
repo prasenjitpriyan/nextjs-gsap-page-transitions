e5dabe33468bfef6e2ad422a9ecf941fd3ace489
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2025-04-03 08:30:11 +0530

    Project initialize

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section>
+      <h1 className="text-7xl font-extrabold text-center mt-10">Welcome</h1>
+    </section>
+  )
+}
+
+export default Home
